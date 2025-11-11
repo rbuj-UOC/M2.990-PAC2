d4e32d44841dc8ec4891a860a301699a9fa31bea
Robert Buj <rbuj@users.noreply.github.com>
2025-11-11 11:10:41 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Second Assignment - Hypothesis Testing
+
+## Install Required Libraries
+
+To install the required R libraries, run the following command in your R
+console:
+
+```r
+source("install.R")
+```
+
+To install Pandoc on macOS, use Homebrew:
+
+```shell
+brew install pandoc
+```
+
+## Unzip datasource
+
+To unzip the datasource, run the following command in your terminal:
+
+```shell
+unzip data.zip
+```
---
+++ install.R
@@ -0,0 +1,11 @@
+# Install required packages for R-cast course
+install.packages(c(
+  "languageserver", # for R language server support for VSCode
+  "readr", # for reading CSV files
+  "dplyr", # for data manipulation
+  "rmarkdown", # for dynamic report generation
+  "knitr", # for dynamic report generation
+  "ggplot2", # for data visualization
+  "kableExtra", # for enhanced table formatting
+  "tidyr" # for data tidying
+))
